cee3f1b46ddc86fa8d677713cb12e1b16f5b8f34
Labib2003 <labib.salimi30@gmail.com>
2026-04-11 14:01:22 +0600

    lsp and linter added

--- init.lua
+++ init.lua
@@ -79,24 +79,28 @@ vim.opt.redrawtime = 10000 -- increase neovim redraw tolerance
 vim.opt.maxmempattern = 20000 -- increase max memory
 
 -- KEYMAPS FOR BULIT IN FEATURES
-vim.g.mapleader = " " 
-vim.g.maplocalleader = " " 
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 vim.keymap.set("n", "<leader>q", "<cmd>q<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<leader>w", "<cmd>w<CR>", { noremap = true, silent = true })
 
 vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]])
 
-
 -- CUSTOM PLUGINS
 vim.pack.add({
-    "https://github.com/akinsho/toggleterm.nvim",
+	"https://github.com/akinsho/toggleterm.nvim",
 	"https://www.github.com/lewis6991/gitsigns.nvim",
-    "https://github.com/nvim-tree/nvim-web-devicons",
-    "https://github.com/nvim-tree/nvim-tree.lua",
+	"https://github.com/nvim-tree/nvim-web-devicons",
+	"https://github.com/nvim-tree/nvim-tree.lua",
 	"https://www.github.com/ibhagwan/fzf-lua",
 	"https://www.github.com/echasnovski/mini.nvim",
-    "https://github.com/akinsho/bufferline.nvim"
+	"https://github.com/akinsho/bufferline.nvim",
+	"https://github.com/nvim-treesitter/nvim-treesitter",
+	"https://www.github.com/neovim/nvim-lspconfig",
+	"https://github.com/mason-org/mason.nvim",
+	"https://github.com/creativenull/efmls-configs-nvim",
+	"https://github.com/saghen/blink.cmp",
 })
 
 local function packadd(name)
@@ -109,25 +113,30 @@ packadd("nvim-tree.lua")
 packadd("fzf-lua")
 packadd("mini.nvim")
 packadd("bufferline.nvim")
+packadd("nvim-treesitter")
+packadd("nvim-lspconfig")
+packadd("mason.nvim")
+packadd("efmls-configs-nvim")
+packadd("blink.cmp")
 
 -- PLUGIN CONFIG
 require("toggleterm").setup({
-  direction = "float",
-  start_in_insert = true,
+	direction = "float",
+	start_in_insert = true,
 
-  on_open = function(term)
-    vim.schedule(function()
-      vim.cmd("startinsert!")
-    end)
-  end, 
+	on_open = function(term)
+		vim.schedule(function()
+			vim.cmd("startinsert!")
+		end)
+	end,
 
-  size = function(term)
-    if term.direction == "horizontal" then
-      return 15
-    elseif term.direction == "vertical" then
-      return 35 
-    end
-  end,
+	size = function(term)
+		if term.direction == "horizontal" then
+			return 15
+		elseif term.direction == "vertical" then
+			return 35
+		end
+	end,
 })
 vim.keymap.set("n", "<leader>t", "<cmd>ToggleTerm direction=vertical<CR>", { silent = true })
 
@@ -154,11 +163,11 @@ require("nvim-tree").setup({
 	renderer = {
 		group_empty = true,
 	},
-    actions = {  
-    change_dir = {  
-      restrict_above_cwd = true,  
-    },  
-  },
+	actions = {
+		change_dir = {
+			restrict_above_cwd = true,
+		},
+	},
 })
 vim.keymap.set("n", "<leader>e", function()
 	require("nvim-tree.api").tree.toggle()
@@ -188,26 +197,26 @@ end, { desc = "FZF Diagnostics Workspace" })
 local Terminal = require("toggleterm.terminal").Terminal
 
 local lazygit = Terminal:new({
-  cmd = "lazygit",
-  hidden = true,
-  direction = "float",
+	cmd = "lazygit",
+	hidden = true,
+	direction = "float",
 
-  float_opts = {
-    border = "double",
-  },
+	float_opts = {
+		border = "double",
+	},
 
-  on_open = function(term)
-    vim.cmd("startinsert!")
-    vim.keymap.set("n", "q", "<cmd>close<CR>", { buffer = term.bufnr, silent = true })
-  end,
+	on_open = function(term)
+		vim.cmd("startinsert!")
+		vim.keymap.set("n", "q", "<cmd>close<CR>", { buffer = term.bufnr, silent = true })
+	end,
 
-  on_close = function()
-    vim.cmd("startinsert!")
-  end,
+	on_close = function()
+		vim.cmd("startinsert!")
+	end,
 })
 
 function _lazygit_toggle()
-  lazygit:toggle()
+	lazygit:toggle()
 end
 vim.keymap.set("n", "<leader>gg", _lazygit_toggle, { noremap = true, silent = true })
 
@@ -217,53 +226,269 @@ require("mini.comment").setup({})
 require("mini.pairs").setup({})
 require("mini.basics").setup({})
 require("mini.icons").setup({})
-require('mini.statusline').setup({  
-  content = {  
-    active = function()  
-      -- Add offset for nvimtree  
-      if vim.bo.filetype == 'NvimTree' then  
-        return '   ' .. MiniStatusline.combine_groups({  
-          { hl = 'MiniStatuslineFilename', strings = { 'NvimTree' } },  
-          '%='  
-        })  
-      end  
-        
-      -- Default content for other buffers  
-      local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })  
-      local git           = MiniStatusline.section_git({ trunc_width = 40 })  
-      local diff          = MiniStatusline.section_diff({ trunc_width = 75 })  
-      local diagnostics   = MiniStatusline.section_diagnostics({ trunc_width = 75 })  
-      local lsp           = MiniStatusline.section_lsp({ trunc_width = 75 })  
-      local filename      = MiniStatusline.section_filename({ trunc_width = 140 })  
-      local fileinfo      = MiniStatusline.section_fileinfo({ trunc_width = 120 })  
-      local location      = MiniStatusline.section_location({ trunc_width = 75 })  
-      local search        = MiniStatusline.section_searchcount({ trunc_width = 75 })  
-  
-      return MiniStatusline.combine_groups({  
-        { hl = mode_hl,                  strings = { mode } },  
-        { hl = 'MiniStatuslineDevinfo',  strings = { git, diff, diagnostics, lsp } },  
-        '%<',  
-        { hl = 'MiniStatuslineFilename', strings = { filename } },  
-        '%=',  
-        { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },  
-        { hl = mode_hl,                  strings = { search, location } },  
-      })  
-    end  
-  }  
+require("mini.statusline").setup({
+	content = {
+		active = function()
+			-- Add offset for nvimtree
+			if vim.bo.filetype == "NvimTree" then
+				return "   "
+					.. MiniStatusline.combine_groups({
+						{ hl = "MiniStatuslineFilename", strings = { "NvimTree" } },
+						"%=",
+					})
+			end
+
+			-- Default content for other buffers
+			local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
+			local git = MiniStatusline.section_git({ trunc_width = 40 })
+			local diff = MiniStatusline.section_diff({ trunc_width = 75 })
+			local diagnostics = MiniStatusline.section_diagnostics({ trunc_width = 75 })
+			local lsp = MiniStatusline.section_lsp({ trunc_width = 75 })
+			local filename = MiniStatusline.section_filename({ trunc_width = 140 })
+			local fileinfo = MiniStatusline.section_fileinfo({ trunc_width = 120 })
+			local location = MiniStatusline.section_location({ trunc_width = 75 })
+			local search = MiniStatusline.section_searchcount({ trunc_width = 75 })
+
+			return MiniStatusline.combine_groups({
+				{ hl = mode_hl, strings = { mode } },
+				{ hl = "MiniStatuslineDevinfo", strings = { git, diff, diagnostics, lsp } },
+				"%<",
+				{ hl = "MiniStatuslineFilename", strings = { filename } },
+				"%=",
+				{ hl = "MiniStatuslineFileinfo", strings = { fileinfo } },
+				{ hl = mode_hl, strings = { search, location } },
+			})
+		end,
+	},
 })
 
 require("bufferline").setup({
-  options = {
-    offsets = {
-      {
-        filetype = "NvimTree",
-        text = "File Explorer",
-        text_align = "left",
-      }
-    }
-  }
+	options = {
+		offsets = {
+			{
+				filetype = "NvimTree",
+				text = "File Explorer",
+				text_align = "left",
+			},
+		},
+	},
 })
 vim.keymap.set("n", "<leader>bb", "<cmd>BufferLinePick<CR>", { noremap = true, silent = true })
+
+local setup_treesitter = function()
+	local treesitter = require("nvim-treesitter")
+	treesitter.setup({})
+	local ensure_installed = {
+		"lua",
+	}
+
+	local config = require("nvim-treesitter.config")
+
+	local already_installed = config.get_installed()
+	local parsers_to_install = {}
+
+	for _, parser in ipairs(ensure_installed) do
+		if not vim.tbl_contains(already_installed, parser) then
+			table.insert(parsers_to_install, parser)
+		end
+	end
+
+	if #parsers_to_install > 0 then
+		treesitter.install(parsers_to_install)
+	end
+
+	local group = vim.api.nvim_create_augroup("TreeSitterConfig", { clear = true })
+	vim.api.nvim_create_autocmd("FileType", {
+		group = group,
+		callback = function(args)
+			if vim.list_contains(treesitter.get_installed(), vim.treesitter.language.get_lang(args.match)) then
+				vim.treesitter.start(args.buf)
+			end
+		end,
+	})
+end
+
+setup_treesitter()
+
+require("mason").setup({})
+
+local diagnostic_signs = {
+	Error = "",
+	Warn = "",
+	Hint = "",
+	Info = "",
+}
+
+vim.diagnostic.config({
+	virtual_text = { prefix = "●", spacing = 4 },
+	signs = {
+		text = {
+			[vim.diagnostic.severity.ERROR] = diagnostic_signs.Error,
+			[vim.diagnostic.severity.WARN] = diagnostic_signs.Warn,
+			[vim.diagnostic.severity.INFO] = diagnostic_signs.Info,
+			[vim.diagnostic.severity.HINT] = diagnostic_signs.Hint,
+		},
+	},
+	underline = true,
+	update_in_insert = false,
+	severity_sort = true,
+	float = {
+		border = "rounded",
+		source = "always",
+		header = "",
+		prefix = "",
+		focusable = false,
+		style = "minimal",
+	},
+})
+
+do
+	local orig = vim.lsp.util.open_floating_preview
+	function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+		opts = opts or {}
+		opts.border = opts.border or "rounded"
+		return orig(contents, syntax, opts, ...)
+	end
+end
+
+local function lsp_on_attach(ev)
+	local client = vim.lsp.get_client_by_id(ev.data.client_id)
+	if not client then
+		return
+	end
+
+	local bufnr = ev.buf
+	local opts = { noremap = true, silent = true, buffer = bufnr }
+
+	vim.keymap.set("n", "<leader>gd", function()
+		require("fzf-lua").lsp_definitions({ jump_to_single_result = true })
+	end, opts)
+
+	vim.keymap.set("n", "<leader>gD", vim.lsp.buf.definition, opts)
+
+	vim.keymap.set("n", "<leader>gS", function()
+		vim.cmd("vsplit")
+		vim.lsp.buf.definition()
+	end, opts)
+
+	vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+	vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+
+	vim.keymap.set("n", "<leader>D", function()
+		vim.diagnostic.open_float({ scope = "line" })
+	end, opts)
+	vim.keymap.set("n", "<leader>d", function()
+		vim.diagnostic.open_float({ scope = "cursor" })
+	end, opts)
+	vim.keymap.set("n", "<leader>nd", function()
+		vim.diagnostic.jump({ count = 1 })
+	end, opts)
+
+	vim.keymap.set("n", "<leader>pd", function()
+		vim.diagnostic.jump({ count = -1 })
+	end, opts)
+
+	vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+
+	vim.keymap.set("n", "<leader>fd", function()
+		require("fzf-lua").lsp_definitions({ jump_to_single_result = true })
+	end, opts)
+	vim.keymap.set("n", "<leader>fr", function()
+		require("fzf-lua").lsp_references()
+	end, opts)
+	vim.keymap.set("n", "<leader>ft", function()
+		require("fzf-lua").lsp_typedefs()
+	end, opts)
+	vim.keymap.set("n", "<leader>fs", function()
+		require("fzf-lua").lsp_document_symbols()
+	end, opts)
+	vim.keymap.set("n", "<leader>fw", function()
+		require("fzf-lua").lsp_workspace_symbols()
+	end, opts)
+	vim.keymap.set("n", "<leader>fi", function()
+		require("fzf-lua").lsp_implementations()
+	end, opts)
+
+	if client:supports_method("textDocument/codeAction", bufnr) then
+		vim.keymap.set("n", "<leader>oi", function()
+			vim.lsp.buf.code_action({
+				context = { only = { "source.organizeImports" }, diagnostics = {} },
+				apply = true,
+				bufnr = bufnr,
+			})
+			vim.defer_fn(function()
+				vim.lsp.buf.format({ bufnr = bufnr })
+			end, 50)
+		end, opts)
+	end
+end
+
+vim.api.nvim_create_autocmd("LspAttach", { group = augroup, callback = lsp_on_attach })
+
+-- vim.keymap.set("n", "<leader>q", function()
+-- 	vim.diagnostic.setloclist({ open = true })
+-- end, { desc = "Open diagnostic list" })
+vim.keymap.set("n", "<leader>dl", vim.diagnostic.open_float, { desc = "Show line diagnostics" })
+
+require("blink.cmp").setup({
+	keymap = {
+		preset = "none",
+		["<C-Space>"] = { "show", "hide" },
+		["<CR>"] = { "accept", "fallback" },
+		["<C-j>"] = { "select_next", "fallback" },
+		["<C-k>"] = { "select_prev", "fallback" },
+		["<Tab>"] = { "snippet_forward", "fallback" },
+		["<S-Tab>"] = { "snippet_backward", "fallback" },
+	},
+	appearance = { nerd_font_variant = "mono" },
+	completion = { menu = { auto_show = true } },
+	sources = { default = { "lsp", "path", "buffer", "snippets" } },
+	snippets = {
+		expand = function(snippet)
+			require("luasnip").lsp_expand(snippet)
+		end,
+	},
+
+	fuzzy = {
+		implementation = "prefer_rust",
+		prebuilt_binaries = { download = true },
+	},
+})
+
+vim.lsp.config["*"] = {
+	capabilities = require("blink.cmp").get_lsp_capabilities(),
+}
+
+vim.lsp.config("lua_ls", {
+	settings = {
+		Lua = {
+			diagnostics = { globals = { "vim" } },
+			telemetry = { enable = false },
+		},
+	},
+})
+
+do
+	local luacheck = require("efmls-configs.linters.luacheck")
+	local stylua = require("efmls-configs.formatters.stylua")
+
+	vim.lsp.config("efm", {
+		filetypes = {
+			"lua",
+		},
+		init_options = { documentFormatting = true },
+		settings = {
+			languages = {
+				lua = { luacheck, stylua },
+			},
+		},
+	})
+end
+
+vim.lsp.enable({
+	"lua_ls",
+	"efm",
+})
 
 -- AUTOCMDS
 -- highlight yanked text
@@ -292,5 +517,44 @@ vim.api.nvim_create_autocmd("BufReadPost", {
 		end
 
 		pcall(vim.api.nvim_win_set_cursor, 0, last_pos)
+	end,
+})
+
+-- Format on save (ONLY real file buffers, ONLY when efm is attached)
+vim.api.nvim_create_autocmd("BufWritePre", {
+	group = augroup,
+	pattern = {
+		"*.lua",
+	},
+	callback = function(args)
+		-- avoid formatting non-file buffers (helps prevent weird write prompts)
+		if vim.bo[args.buf].buftype ~= "" then
+			return
+		end
+		if not vim.bo[args.buf].modifiable then
+			return
+		end
+		if vim.api.nvim_buf_get_name(args.buf) == "" then
+			return
+		end
+
+		local has_efm = false
+		for _, c in ipairs(vim.lsp.get_clients({ bufnr = args.buf })) do
+			if c.name == "efm" then
+				has_efm = true
+				break
+			end
+		end
+		if not has_efm then
+			return
+		end
+
+		pcall(vim.lsp.buf.format, {
+			bufnr = args.buf,
+			timeout_ms = 2000,
+			filter = function(c)
+				return c.name == "efm"
+			end,
+		})
 	end,
 })
